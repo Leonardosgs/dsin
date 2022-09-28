1b300b90d4d97b5243da8a6866f25131f2b80a5a
Leonardosgs <leonardosgs@hotmail.com>
2022-09-28 00:08:58 -0400

    Teste DSIN

--- teste-desin/src/questao5/Principal5.java
+++ teste-desin/src/questao5/Principal5.java
@@ -8,8 +8,19 @@ public class Principal5 {
 		
 		Scanner s = new Scanner(System.in);
 		
-		System.out.println(Verificador.validadorPlaca("ABC-1234"));
+		String placa = "ABC-1234";
+		String placa2 = "ABC-1C34";
 		
+		if (Verificador.validadorPlaca(placa)) {
+			System.out.println("Para a placa: " + placa.replaceAll("[^a-zA-Z0-9]", "") );
+			System.out.println("Padrão: Brasil");
+			System.out.println("Correspondente: " + placa.replaceAll("[^a-zA-Z0-9]", "").replace(placa.charAt(5), placa.charAt(2)));
+		}
+		if (Verificador.validadorPlaca(placa2)) {
+			System.out.println("Para a placa: " + placa2.replaceAll("[^a-zA-Z0-9]", "") );
+			System.out.println("Padrão: Mercosul");
+			System.out.println("Correspondente: " + placa2.replaceAll("[^a-zA-Z0-9]", "").replace(placa2.charAt(5), '2'));
+		}
 		
 		
 		s.close();
--- teste-desin/src/questao5/Verificador.java
+++ teste-desin/src/questao5/Verificador.java
@@ -7,11 +7,11 @@ public class Verificador {
 	
 	public static boolean validadorPlaca(String placa) {
 		placa = placa.replaceAll("[^a-zA-Z0-9]", "");
-		if(placa.length() != 7){
-			if(placa.substring(0, 3).matches("[A-Z]*") && placa.substring(4).matches("[0-9]*") && placa.substring(5).matches("[A-Z]*") && placa.substring(6, 7).matches("[0-9]*")){
-				   return true;
-				}
-			if(placa.substring(0, 3).matches("[A-Z]*") && placa.substring(4, 7).matches("[0-9]*")) {
+		if(placa.length() == 7){
+			if(placa.substring(0, 3).matches("[A-Z]*") && placa.substring(3,4).matches("[0-9]*") && placa.substring(4,5).matches("[A-Z]*") && placa.substring(5, 7).matches("[0-9]*")){
+				return true;
+			}
+			if(placa.substring(0, 3).matches("[A-Z]*") && placa.substring(3, 6).matches("[0-9]*")) {
 				return true;
 			}
 		   }
